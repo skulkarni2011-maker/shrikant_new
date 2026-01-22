49caef6788ed6c816997307de6fb7e3b4811633b
skulkarni2011-maker <skulkarni2011@gmail.com>
2026-01-22 19:35:02 +0530

    Add welcome message for DevOps day 3 session

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class DevOpsWelcome {
     public static void main(String[] args) {
         System.out.println("Welcome to DevOps day 2 session");
+        System.out.println("Welcome to DevOps day 3 session");
     }
 }
